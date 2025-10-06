6624a5ad9fbeda8c5db324cdf82c8d4abcbd769f
Kidpech Pianpithak <85755801+Kidpech-code@users.noreply.github.com>
2025-10-06 18:09:35 +0700

    Restore legacy Riverpod import in theme_controller.dart

--- lib/src/theme/theme_controller.dart
+++ lib/src/theme/theme_controller.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 import 'package:theme_setup/src/services/storage_service.dart';
 import 'package:theme_setup/src/theme/theme_models.dart';
 
