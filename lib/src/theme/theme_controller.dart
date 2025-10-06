7fee09a15717bffad980039631222d2d90125777
Kidpech Pianpithak <85755801+Kidpech-code@users.noreply.github.com>
2025-10-06 18:01:14 +0700

    Restore legacy Riverpod import

--- lib/src/theme/theme_controller.dart
+++ lib/src/theme/theme_controller.dart
@@ -3,7 +3,8 @@ import 'package:flutter_riverpod/legacy.dart';
 import 'package:theme_setup/src/services/storage_service.dart';
 import 'package:theme_setup/src/theme/theme_models.dart';
 
-final themeControllerProvider = StateNotifierProvider<ThemeController, ThemeSettings>((ref) => ThemeController());
+final themeControllerProvider =
+    StateNotifierProvider<ThemeController, ThemeSettings>((ref) => ThemeController());
 
 class ThemeController extends StateNotifier<ThemeSettings> {
   static const _storageKey = 'theme_settings';
@@ -15,9 +16,7 @@ class ThemeController extends StateNotifier<ThemeSettings> {
     if (raw is Map<String, dynamic>) {
       return ThemeSettings.fromJson(raw);
     }
-    // default: system + blue seed color
-    // ignore: deprecated_member_use
-    return ThemeSettings(mode: AppThemeMode.system, seedColorValue: Colors.blue.value);
+    return ThemeSettings.fallback();
   }
 
   Future<void> _save() async {
@@ -30,7 +29,6 @@ class ThemeController extends StateNotifier<ThemeSettings> {
   }
 
   void setSeedColor(Color color) {
-    // ignore: deprecated_member_use
     state = ThemeSettings(mode: state.mode, seedColorValue: color.value);
     _save();
   }
